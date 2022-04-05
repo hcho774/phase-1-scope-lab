8571c4b10f91d0957d9db1576ef77c1bf5db60b6
Hyun Sung Cho <hcho774@gmail.com>
2022-04-05 11:55:52 -0400

    commit tree 1391c8003f6adadf890da8c65a3b3c36d6c2c4c0

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+
+
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase(); /// === "BOB"
+}
+
+console.log(customerName);
+
+function setBestCustomer() {
+    bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = "maybe bob"
+}
+
+const leastFavoriteCustomer = "cho";
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "unsuccessfully tries to reassign the least favorite customer";
+}
